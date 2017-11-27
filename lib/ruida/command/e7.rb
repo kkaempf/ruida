72cd89de87ecbfef661672482ad39e182fa9121e
Klaus Kämpf <kkaempf@suse.de>
2017-11-27 21:02:32 +0100

    Feeding

--- lib/ruida/command/e7.rb
+++ lib/ruida/command/e7.rb
@@ -5,7 +5,7 @@ module Ruida
         0x03 => ["Top_Left_E7_07", :abs, :abs],
         0x04 => ["E7 04", -9, :abs],
         0x05 => ["Speed_E7_05", -4, :speed],
-        0x06 => ["E7 06", :abs, :abs],
+        0x06 => ["Feeding", :abs, :abs], # Feeding1, Distance+Feeding2
         0x07 => ["Bottom_Right_E7_07", :abs, :abs],
         0x08 => ["E7 08", -9, :abs],
         0x13 => ["E7 13", :abs, :abs],
--- lib/ruida/command/f1.rb
+++ lib/ruida/command/f1.rb
@@ -5,7 +5,8 @@ module Ruida
 	0x00 => ["F1 00", -1 ],
 	0x01 => ["F1 01", -1 ],
 	0x02 => ["F1 02", -1 ],
-	0x03 => ["Laser2_Offset", :abs, :abs ]
+	0x03 => ["Laser2_Offset", :abs, :abs ],
+	0x04 => ["Enable_Feeding(auto?)", :bool ]
       }
     end
   end
