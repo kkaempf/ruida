7546cd49982725e8af04b4214dce56c3bab46a83
Klaus Kämpf <kkaempf@suse.de>
2017-11-27 20:00:15 +0100

    Laser 3 and 4 power settings

--- lib/ruida/command/c6.rb
+++ lib/ruida/command/c6.rb
@@ -3,10 +3,10 @@ module Ruida
     def self.format
       { 0x01 => ["Laser_1_Min_Pow_C6_01", :power],
         0x02 => ["Laser_1_Max_Pow_C6_02", :power],
-        0x05 => ["C6 05", :power],
-        0x06 => ["C6 06", :power],
-        0x07 => ["C6 07", :power],
-        0x08 => ["C6 08", :power],
+        0x05 => ["Laser_3_Min_Pow_C6_05", :power],
+        0x06 => ["Laser_3_Max_Pow_C6_06", :power],
+        0x07 => ["Laser_4_Min_Pow_C6_07", :power],
+        0x08 => ["Laser_4_Max_Pow_C6_08", :power],
         0x10 => ["C6 10", :abs],
         0x12 => ["Cut_Open_delay", :ms],
         0x13 => ["Cut_Close_delay", :ms],
@@ -16,16 +16,16 @@ module Ruida
         0x22 => ["Laser_2_Max_Pow_C6_22", :power],
         0x31 => ["Laser_1_Min_Pow_C6_31", :layer, :power],
         0x32 => ["Laser_1_Max_Pow_C6_32", :layer, :power],
-        0x35 => ["C6 35", :layer, :power],
-        0x36 => ["C6 36", :layer, :power],
-        0x37 => ["C6 37", :layer, :power],
-        0x38 => ["C6 38", :layer, :power],
+        0x35 => ["Laser_3_Min_Pow_C6_35", :layer, :power], # 654XG only
+        0x36 => ["Laser_3_Max_Pow_C6_36", :layer, :power], # 654XG only
+        0x37 => ["Laser_4_Min_Pow_C6_37", :layer, :power], # 654XG only
+        0x38 => ["Laser_4_Max_Pow_C6_38", :layer, :power], # 654XG only
         0x41 => ["Laser_2_Min_Pow_C6_41", :layer, :power],
         0x42 => ["Laser_2_Max_Pow_C6_42", :layer, :power],
         0x50 => ["Cut_through_power1", :power],
         0x51 => ["Cut_through_power2", :power],
-        0x55 => ["C6 55", :power],
-        0x56 => ["C6 56", :power]
+        0x55 => ["Cut_through_power3", :power],
+        0x56 => ["Cut_through_power4", :power]
     }
     end
   end
