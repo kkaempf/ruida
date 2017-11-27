bd9c41c84aa218703902ae4ba0d0a88f51684ba2
Klaus Kämpf <kkaempf@suse.de>
2017-11-27 21:40:38 +0100

    Bottom right

--- lib/ruida/command/e7.rb
+++ lib/ruida/command/e7.rb
@@ -7,20 +7,20 @@ module Ruida
         0x05 => ["Speed_E7_05", -4, :speed],
         0x06 => ["Feeding", :abs, :abs], # Feeding1, Distance+Feeding2
         0x07 => ["Bottom_Right_E7_07", :abs, :abs],
-        0x08 => ["E7 08", -9, :abs],
+        0x08 => ["Bottom_Right_E7_08", -4, :abs, :abs],
         0x13 => ["E7 13", :abs, :abs],
-        0x17 => ["Point_E7_17", :abs, :abs],
+        0x17 => ["Bottom_Right_E7_17", :abs, :abs],
         0x23 => ["E7 23", :abs, :abs],
         0x24 => ["E7 24", -1],
         0x50 => ["Top_Left_E7_50", :abs, :abs],
         0x51 => ["Bottom_Right_E7_51", :abs, :abs],
         0x52 => ["E7 52", 0x00, :abs, :abs],
-        0x53 => ["Point_E7_53", 0x00, :abs, :abs],
+        0x53 => ["Bottom_Right_E7_53", 0x00, :abs, :abs],
         0x54 => ["Pen_Draw_Y", -1, :abs],
         0x55 => ["Laser2_Y_Offset", :bool, :abs],
         0x60 => ["E7 60", -1],
         0x61 => ["E7 61", 0x00, :abs, :abs],
-        0x62 => ["Point_E7_62", 0x00, :abs, :abs]
+        0x62 => ["Bottom_Right_E7_62", 0x00, :abs, :abs]
       }
     end
   end
--- lib/ruida/command/f2.rb
+++ lib/ruida/command/f2.rb
@@ -6,8 +6,8 @@ module Ruida
 	0x01 => ["F2 01", -1 ],
 	0x02 => ["F2 02", -10 ],
 	0x03 => ["F2 03", :abs, :abs ],
-	0x04 => ["Point_F2", :abs, :abs ],
-	0x05 => ["F2 05", -9, :abs ],
+	0x04 => ["Bottom_Right_F2_04", :abs, :abs ],
+	0x05 => ["Bottom_Right_F2_05", -4, :abs, :abs ],
 	0x06 => ["F2 06", :abs, :abs ],
 	0x07 => ["F2 07", -1 ]
       }
