6f2b07e09b4929558f3f180d796432b2c940d186
Klaus Kämpf <kkaempf@suse.de>
2017-11-29 21:29:53 +0100

    Start commands

--- lib/ruida/command/f1.rb
+++ lib/ruida/command/f1.rb
@@ -2,9 +2,9 @@ module Ruida
   class Cmd_f1 < Cmd
     def self.format
       {
-	0x00 => ["F1 00", -1 ],
-	0x01 => ["F1 01", -1 ],
-	0x02 => ["F1 02", -1 ],
+	0x00 => ["Start0", 0x00 ],
+	0x01 => ["Start1", 0x00 ],
+	0x02 => ["Start2", 0x00 ],
 	0x03 => ["Laser2_Offset", :abs, :abs ],
 	0x04 => ["Enable_Feeding(auto?)", :bool ]
       }
