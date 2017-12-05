5cbbd92d51d092410a7bed7bfe33a1e2d2668597
Klaus Kämpf <kkaempf@suse.de>
2017-12-05 12:04:35 +0100

    Doc: generate command documentation in markdown

--- lib/ruida/command.rb
+++ lib/ruida/command.rb
@@ -1,13 +1,16 @@
 module Ruida
   class Command
     require "ruida/command/cmd"
+    def self.klass_for hex
+      require "ruida/command/"+hex
+      klass = Ruida::const_get "Cmd_"+hex
+    end
     def self.consume data
       cmd = data.consume
       return nil unless cmd
       hex = sprintf "%02x", cmd
       begin
-        require "ruida/command/"+hex
-        klass = Ruida::const_get "Cmd_"+hex
+        klass = self.klass_for hex
         cmd = klass.new data if klass
         cmd.interprete
         cmd
