d3b2a77e3d413aa7d375f1a443c80f8dd29d2db5
Klaus Kämpf <kkaempf@suse.de>
2018-01-03 18:16:06 +0100

    fix D8 command

--- lib/ruida/command/d8.rb
+++ lib/ruida/command/d8.rb
@@ -2,7 +2,7 @@ module Ruida
   class Cmd_d8 < Cmd
     def self.format
       {
-        0x00 => ["Light_RED", 0x00],
+        0x00 => ["Light_RED"],
         0x12 => ["UploadFollows"],
       }
     end
